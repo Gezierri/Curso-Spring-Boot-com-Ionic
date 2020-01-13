36f60cfff1407eda138fdf79553b13c69f2a9948
gezierri <gezierrismaniotto@gmail.com>
2020-01-13 20:55:46 -0300

    Listando todas as Categorias com DTO

--- src/main/java/com/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.cursomc.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.cursomc.domain.Categoria;
+import com.cursomc.dto.CategoriaDTO;
 import com.cursomc.service.CategoriaService;
 
 @RestController
@@ -24,6 +27,13 @@ public class CategoriaResource {
 
 	@Autowired
 	private CategoriaService categoriaService;
+	
+	@GetMapping
+	public ResponseEntity<List<CategoriaDTO>> listAll(){
+		List<Categoria> lista = categoriaService.findAll();
+		List<CategoriaDTO> listDto = lista.stream().map(x -> new CategoriaDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
+	}
 	
 	@GetMapping("/{id}")
 	public ResponseEntity<Categoria> find(@PathVariable Integer id) {
--- src/main/java/com/cursomc/service/CategoriaService.java
+++ src/main/java/com/cursomc/service/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.cursomc.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,6 +42,10 @@ public class CategoriaService {
 		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrationException("Não é possível deletar uma categoria  que possui produtos!");
 		}
+	}
+
+	public List<Categoria> findAll() {
+		return categoriaRepository.findAll();
 	}
 
 }
