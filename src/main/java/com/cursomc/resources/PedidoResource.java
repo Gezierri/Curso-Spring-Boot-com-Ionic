1380e7f2c831d3566e5694171628020b5a592ecd
Gezierri <gezierri@gmail.com>
2020-03-10 14:47:33 -0300

    Retricao de conteudo: cliente so recupera seus pedidos

--- src/main/java/com/cursomc/resources/PedidoResource.java
+++ src/main/java/com/cursomc/resources/PedidoResource.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -25,22 +27,33 @@ public class PedidoResource {
 
 	@Autowired
 	private PedidoService pedidoService;
-	
+
 	@GetMapping("/{id}")
 	public ResponseEntity<Pedido> find(@PathVariable Integer id) {
 		Pedido pedidoSalvo = pedidoService.find(id);
 		return ResponseEntity.status(HttpStatus.OK).body(pedidoSalvo);
 	}
-	
-	@GetMapping
-	public ResponseEntity<List<Pedido>>list(){
-		List<Pedido> list = pedidoService.findAll();
-		return ResponseEntity.ok().body(list);
-	}
+
+//	@GetMapping
+//	public ResponseEntity<List<Pedido>> list() {
+//		List<Pedido> list = pedidoService.findAll();
+//		return ResponseEntity.ok().body(list);
+//	}
+
 	@PostMapping
-	public ResponseEntity<Pedido> inser(@Valid @RequestBody Pedido pedido){
+	public ResponseEntity<Pedido> inser(@Valid @RequestBody Pedido pedido) {
 		pedido = pedidoService.insert(pedido);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(pedido.getId()).toUri();
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(pedido.getId())
+				.toUri();
 		return ResponseEntity.created(uri).build();
+	}
+
+	@GetMapping
+	public ResponseEntity<Page<Pedido>> findPage(@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "instante") String direction,
+			@RequestParam(value = "direction", defaultValue = "DESC") String orderBy) {
+		Page<Pedido> list = pedidoService.findPage(page, linesPerPage, direction, orderBy);
+		return ResponseEntity.ok(list);
 	}
 }
